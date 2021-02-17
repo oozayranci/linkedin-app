949ba99cf868f2fe1ae1d44edd2f056e67821ce4
oozaryanci <oozayranci@gmail.com>
2021-02-17 17:25:57 +0200

    Login component style implementation

--- src/Login.js
+++ src/Login.js
@@ -4,7 +4,13 @@ import './Login.css'
 function Login() {
     return (
         <div className="login">
-           <h1>You are not logged in</h1> 
+           <img src="https://hbr.org/resources/images/article_assets/2020/04/Apr20_14_LinkedInHBR1.jpg"
+           alt=""/>
+
+           <form>
+               <input placeholder="Full name (required if registering)" 
+               type="text"/>
+           </form>
         </div>
     )
 }
